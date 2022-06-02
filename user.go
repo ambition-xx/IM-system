c930aee9b3aa522896f2f8bc1e05ba9c5386bfc2
ambition <ambition.xx@qq.com>
2022-06-02 21:02:23 +0800

    v6 修改用户名

--- user.go
+++ user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"strings"
 )
 
 type User struct {
@@ -9,7 +10,7 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
-	
+
 	// 当前user属于那个server
 	server *Server
 }
@@ -53,26 +54,44 @@ func (this *User) Offline() {
 	this.server.BroadCast(this, "下线")
 }
 
-func (this *User)SendMsg(msg string){
+func (this *User) SendMsg(msg string) {
 	this.conn.Write([]byte(msg))
 }
+
 // 用户处理消息业务
 func (this *User) DoMessage(msg string) {
-	if msg == "who"{
+	if msg == "who" {
+		// 查询当前在线用户
 		this.server.mapLock.Lock()
-		for _, user := range this.server.OnlineMap{
+		for _, user := range this.server.OnlineMap {
 			onilneMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线。。。\n"
 			this.SendMsg(onilneMsg)
 
-		} 
+		}
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式"rename|zhang3"
+		newName := strings.Split(msg, "|")[1]
+
+		//检查newName是否已经存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("用户名已经被使用！")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+			this.Name = newName
+			this.SendMsg("您已经更新用户名：" + newName + "\n")
+		} 
+
 	} else {
-
 		this.server.BroadCast(this, msg)
-	}
-	
-}
 
+	}
+
+}
 
 func (this *User) ListenMessage() {
 	for {
@@ -81,4 +100,3 @@ func (this *User) ListenMessage() {
 		this.conn.Write([]byte(msg + "\n"))
 	}
 }
-
