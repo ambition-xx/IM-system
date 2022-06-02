9178283c83f7a43d0d26a5459c05367a6f628b18
ambition <ambition.xx@qq.com>
2022-06-03 00:18:40 +0800

    客户端实现 菜单显示

--- client.go
+++ client.go
@@ -12,6 +12,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int // 表示当前模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -19,6 +20,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag: 999,
 	}
 	// 链接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -30,10 +32,44 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+func (client *Client) Menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag < 4 { 
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("请输入合法范围内的数字。。。")
+		return false
+	}
+}
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.Menu() != true {
+		}
+		switch client.flag {
+		case 1:
+			fmt.Println("公聊模式")
+		case 2:
+			fmt.Println("私聊模式")
+		case 3:
+			fmt.Println("更新用户名")
+		case 0:
+			fmt.Println("退出")
+
+		}
+	}
+}
 var serverIp string
 var serverPort int
+
 // ./client -ip 127.0.0.1 -port 8888
-func init(){
+func init() {
 	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认是127.0.0.1)")
 	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口号(默认是8888)")
 }
@@ -46,5 +82,7 @@ func main() {
 	}
 	fmt.Println("服务器链接成功。。。")
 
-	select {}
+	//  启动客户端业务
+	client.Run()
+	// select {}
 }
