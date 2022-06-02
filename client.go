331515b6608b1f27fa790c01c13cc7025e0acc0c
ambition <ambition.xx@qq.com>
2022-06-02 23:50:13 +0800

    客户端实现 命令行解析

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -28,8 +29,18 @@ func NewClient(serverIp string, serverPort int) *Client {
 	// 返回对象
 	return client
 }
+
+var serverIp string
+var serverPort int
+// ./client -ip 127.0.0.1 -port 8888
+func init(){
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口号(默认是8888)")
+}
 func main() {
-	client := NewClient()
+
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		log.Println("服务器链接失败。。。")
 	}
