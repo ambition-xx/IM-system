1afd37e0268291b9732acfa271f4595159728823
ambition <ambition.xx@qq.com>
2022-06-02 20:24:50 +0800

    v4 用户业务封装

--- server.go
+++ server.go
@@ -11,78 +11,81 @@ import (
 type Server struct {
 	Ip        string
 	Port      int
+	//在线用户列表
 	OnlineMap map[string]*User
 	mapLock   sync.RWMutex
+	//消息广播的channel
 	Message   chan string
 }
-
+// 创建一个server的接口
 // create a server object
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:        ip,
-		Port:      port,
+		Ip:   ip,
+		Port: port,
 		// 在线用户列表
 		OnlineMap: make(map[string]*User),
 		// 广播消息的channel
-		Message:   make(chan string),
+		Message: make(chan string),
 	}
 	return server
 }
+
 // 监听message广播消息channel的goroutine 一旦有消息发送给全部的用户
-func (this *Server) ListenMessager(){
+func (this *Server) ListenMessager() {
 	for {
 		msg := <-this.Message
 		// 将消息发送给全部在线用户
 		this.mapLock.Lock()
-		for _, cli := range this.OnlineMap{ 
+		for _, cli := range this.OnlineMap {
 			cli.C <- msg
 		}
 		this.mapLock.Unlock()
 	}
 }
+
 // 广播消息的方法
 func (this *Server) BroadCast(user *User, msg string) {
 	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
 	this.Message <- sendMsg
 }
+
 func (this *Server) handler(conn net.Conn) {
 	// 处理当前连接的业务
 	// fmt.Println("连接建立成功。。。")
-
-	user := NewUser(conn)
+	user := NewUser(conn, this)
 	// 用户上线 加入OnlineMap
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	user.Online()
 
 	// 广播用户上线消息
-	this.BroadCast(user, "已上线")
+	// this.BroadCast(user, "已上线")
 
 	// 接收客户端消息
 	go func() {
 		buf := make([]byte, 4096)
-		
-		for  {
+
+		for {
 			n, err := conn.Read(buf)
-			if n == 0{
-				this.BroadCast(user,"下线")
-	
-			}
-			if err != nil && err != io.EOF{
-				log.Println("conn read err", err)
+			if n == 0 {
+				// this.BroadCast(user,"下线")
+				user.Offline()
 				return;
+			}
+			if err != nil && err != io.EOF {
+				log.Println("conn read err", err)
+				return
 			}
 			//将得到消息去除'\n'
 			msg := string(buf[:n-1])
 			// 得到消息进行广播
-			this.BroadCast(user, msg)
+			// this.BroadCast(user, msg)
+			//用户针对消息进行处理
+			user.DoMessage(msg)
 		}
 
-
 	}()
-	select {
-
-	}
+	// 当前handler阻塞
+	select {}
 }
 
 // start a server
@@ -105,4 +108,3 @@ func (this *Server) Start() {
 		go this.handler(conn)
 	}
 }
-
--- user.go
+++ user.go
@@ -9,22 +9,55 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+	
+	// 当前user属于那个server
+	server *Server
 }
 
 // create a user
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
 	}
 	// 监听当前user 里面的channel
 	go user.ListenMessage()
 
 	return user
 }
+
+//用户上线业务
+func (this *User) Online() {
+	// 用户上线 加入OnlineMap
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	// 广播用户上线消息
+	this.server.BroadCast(this, "已上线")
+
+}
+
+//用户下线业务
+func (this *User) Offline() {
+	// 用户下线 从OnlineMap中删除user
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播用户下线消息
+	this.server.BroadCast(this, "下线")
+}
+
+// 用户处理消息业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
+}
+
 
 func (this *User) ListenMessage() {
 	for {
